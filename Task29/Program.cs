7ea980d4fc339042a21f2bf24fda1141f3f1dfe2
Ivan <ermolaev-iv@outlook.com>
2023-09-02 23:34:30 +0300

    Task29

---
+++ Task29/Program.cs
@@ -0,0 +1,15 @@
+﻿//Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+//1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+//6, 1, 33 -> [6, 1, 33]
+
+using System.Globalization;
+
+string arr(string message)
+{
+    Console.Write(message);
+    string array = Console.ReadLine()!;
+
+    return array;
+}
+
+Console.WriteLine($"[{arr("Введите массив :")}]");
